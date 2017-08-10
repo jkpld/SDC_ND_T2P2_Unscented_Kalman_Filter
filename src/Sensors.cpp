56de6c619b626dfbd0935ed2287a5933c1fb72b8
James Kapaldo <james.kapaldo@gmail.com>
2017-08-10 19:00:47 -0400

    Update Sensor class and code Radar::state_to_measure

--- src/Sensors.cpp
+++ src/Sensors.cpp
@@ -1,6 +1,6 @@
 #include "Sensors.h"
 #include "Eigen/Dense"
-#include <math.h>
+// #include <math.h>
 #include <iostream>
 
 using Eigen::MatrixXd;
@@ -10,42 +10,42 @@ using Eigen::VectorXd;
 /*   Radar class definitions
 /******************************************************************************/
 Radar::Radar() {
-  sensor_type = Sensor::extended;
-
   R = MatrixXd::Zero(3,3);
   R.diagonal() << 0.09, 0.0009, 0.09;
+  angleParams_ = VectorXd::Zero(3);
+  angleParams_(1) = 1;
 }
 
 Radar::~Radar() {}
 
-VectorXd Radar::meas_state_difference(const VectorXd &meas, const VectorXd &state) {
-  VectorXd dz = meas - state_to_measure(state);
+MatrixXd Radar::state_to_measure(const MatrixXd &SigmaPoints) {
 
-  // Extract the angle
-  double phi = dz(1);
+  // Get the number of states
+  int n = SigmaPoints.cols();
 
-  // Wrap to (-pi, +pi]
-  phi = fmod(phi + M_PI, 2*M_PI);
-  if (phi < 0)
-    phi += 2*M_PI;
-  phi -= M_PI;
+  // extract the needed values
+  ArrayXd rx = state.row(0);
+  ArrayXd ry = state.row(1);
+  ArrayXd v = state.row(2);
+  ArrayXd psi = state.row(3);
 
-  dz(1) = phi;
-  return dz;
-}
+  // compute the velocity
+  ArrayXd vx = v*psi.cos();
+  ArrayXd vy = v*psi.sin();
 
-VectorXd Radar::state_to_measure(const VectorXd &state) {
-  float rx = state(0);
-  float ry = state(1);
-  float vx = state(2);
-  float vy = state(3);
+  // compute the object's polar angle
+  ArrayXd phi = ArrayXd::Zero(n);
+  for (unsigned int ii=0; ii<n; i++) {
+    phi(ii) = atan2(ry(ii),rx(ii));
+  }
 
-  float r = sqrt(rx*rx + ry*ry);
-  float phi = atan2(ry,rx);
-  float rdot = (rx*vx + ry*vy)/r;
+  // initialize the output matrix
+  MatrixXd meas = MatrixXd(3, n);
 
-  VectorXd meas = VectorXd(3);
-  meas << r, phi, rdot;
+  meas.row(0) = sqrt(rx*rx + ry*ry); // r
+  meas.row(1) = phi;
+  meas.row(2) = (rx*vx + ry*vy)/r; // r_dot
+
   return meas;
 }
 
@@ -59,57 +59,15 @@ VectorXd Radar::measure_to_state(const VectorXd &meas) {
   return state;
 }
 
-MatrixXd Radar::Jacobian(const VectorXd &state) {
-  Hj_ = MatrixXd(3,4);
-  //recover state parameters
-	float rx = state(0);
-	float ry = state(1);
-	float vx = state(2);
-	float vy = state(3);
-
-  //pre-compute a set of terms to avoid repeated calculation
-	float r2 = rx*rx + ry*ry;
-	float r = sqrt(r2);
-	float r3 = r2*r;
-
-  //check division by zero
-	if(fabs(r2) < 0.0001){
-		std::cout << "CalculateJacobian () - Error - Division by Zero" << std::endl;
-		return Hj_;
-	}
-
-  float cross = vx*ry - vy*rx;
-
-	//compute the Jacobian matrix
-	Hj_ << (rx/r), (ry/r), 0, 0,
-		  -(ry/r2), (rx/r2), 0, 0,
-		  ry*cross/r3, -rx*cross/r3, rx/r, ry/r;
-
-  return Hj_;
-}
-
 /*******************************************************************************
 /*   Lidar class definitions
 /******************************************************************************/
-Lidar::Lidar()
-{
-  sensor_type = Sensor::linear;
-
+Lidar::Lidar() {
   R = MatrixXd::Zero(2,2);
   R.diagonal() << 0.0225, 0.0225;
-
-  H_ = MatrixXd::Zero(2, 4);
-  H_(0,0) = 1;
-  H_(1,1) = 1;
-
-  Hj_ = H_;
+  angleParams_ = VectorXd::Zero(2);
 }
 Lidar::~Lidar() {}
-
-VectorXd Lidar::meas_state_difference(const VectorXd &meas, const VectorXd &state) {
-  VectorXd dz = meas - state_to_measure(state);
-  return dz;
-}
 
 VectorXd Lidar::measure_to_state(const VectorXd &meas) {
   return H_.transpose() * meas;
