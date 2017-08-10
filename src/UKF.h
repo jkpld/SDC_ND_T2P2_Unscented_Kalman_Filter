55fb839418383afc24bb820525ba702640ca13d1
James Kapaldo <james.kapaldo@gmail.com>
2017-08-10 18:01:29 -0400

    recover lost work and add some utility functions

--- src/UKF.h
+++ src/UKF.h
@@ -11,27 +11,25 @@ public:
 
   VectorXd x_;
   MatrixXd P_;
-  VectorXd isParamAngle_ = (VectorXd(1) << 999999).finished();
-
-  // virtual method for a predict function
-  virtual MatrixXd ProcessModel(MatrixXd SigPnts_aug, double dt) = 0;
 
   /**
-   * Init initializes the uncented kalman filter
+   * Init initializes the unscented kalman filter
    * @param x Initial state
    * @param P Initial state covariance
-   * @param nu process noise vector
-   * @param lambda (optional) lambda for computed sigma points
-   * @param isParamAngle (optional) A VectorXd containig the indices of the
-      state parameters that are angels.
    */
-  void Init(VectorXd& x, MatrixXd& P, VectorXd& nu, double lambda = 999999);
+  void Init(VectorXd& x, MatrixXd& P);
 
   /**
    * Method to set/get the spreading parameter.
    */
   void setLambda(double lambda);
   double getLambda();
+
+  /*
+   * Method to set/get the process noise
+   */
+  void setProcessNoise(VectorXd& nu);
+  VectorXd getProcessNoise();
 
   // Method to preform prediction;
   // @param dt Amount of time we want to predict our state into the future.
@@ -41,9 +39,10 @@ private:
 
   double n_x_ = 0;
   double n_v_ = 0;
-  // VectorXd nu_;
-  double lambda_ = 0; // spreading parameter
   double Lambda_ = sqrt(3); // sqrt(lambda + n)
+
+  VectorXd nu_;
+  VectorXd angleParams_;
 
   VectorXd weights_;
   VectorXd x_aug_;
@@ -52,7 +51,31 @@ private:
   MatrixXd SigPnts_;
   // MatrixXd A_;
 
-  void ComputeSigmaPoints();
+  /*
+   * ComputeSigmaPoints Method to compute the augmented sigma points from the
+   *  current state/covariance
+   */
+  MatrixXd ComputeSigmaPoints();
+
+  /*
+   * labelParametersAsAngle Virtual method to label state parameters as being
+   *  angles
+   */
+  virtual void labelParametersAsAngle() = 0;
+
+  /*
+   * ProcessModel Virtual method that applies the process model to the sigma
+   *  points.
+   * @param SigPnts_aug The augmented sigma points
+   * @param dt Amount of time we want to predict our state into the future.
+   */
+  virtual MatrixXd ProcessModel(MatrixXd SigPnts_aug, double dt) = 0;
+
+  void NormalizeAngles(MatrixXd& M, VectorXd angleParams);
+  void ComputeMeanAndCovariance(MatrixXd& SigmaPoints, VectorXd& x, MatrixXd P);
+  void ComputeMeanAndCovariance(MatrixXd& SigmaPoints, VectorXd& x, MatrixXd P, VectorXd& angleParams);
+  MatrixXd ComputeStateDifference(MatrixXd& SigmaPoints, VectorXd& state);
+  MatrixXd ComputeStateDifference(MatrixXd& SigmaPoints, VectorXd& state, VectorXd& angleParams);
 };
 
 
